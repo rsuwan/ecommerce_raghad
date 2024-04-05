c0b35323944677561b14b41652b6288d9929b8bd
rsuwan <r.r.suwan2001@gmail.com>
2024-04-05 17:29:30 +0300

    wait

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -1,16 +1,16 @@
-import categoriesrouter from './categories/categories.router.js';
-import productsrouter from './products/products.router.js';
+import categoriesRouter from './categories/categories.router.js';
+import productsRouter from './products/products.router.js';
 import connectDB from '../../DB/connection.js';
 const initapp = (app, express) => {
   app.use(express.json());
   connectDB();
   app.get('/', (req, res) => {
-    return res.status(200).json({message:"Welcome.."});
+    return res.status(200).json({ message: "Welcome.." });
   });
-  app.use('/categories',categoriesrouter);
-  app.use('/products',productsrouter);
+  app.use('/categories', categoriesRouter);
+  app.use('/products', productsRouter);
   app.get('*', (req, res) => {
-    return res.status(500).json({message:"Page not found.."});
+    return res.status(500).json({ message: "Page not found.." });
   });
 }
 
--- src/modules/categories/categories.router.js
+++ src/modules/categories/categories.router.js
@@ -7,14 +7,14 @@ router.get('/', categoriesController.getCategories);
 router.get('/active', categoriesController.getactivecategories);
 router.get('/:id', categoriesController.specificcategory);
 router.post(
-  '/',
-  fileUpload(fileValidation.image).single('image'),
-  categoriesController.createcategory
+    '/',
+    fileUpload(fileValidation.image).single('image'),
+    categoriesController.createcategory
 );
 router.put(
-  '/:id',
-  fileUpload(fileValidation.image).single('image'),
-  categoriesController.updatecategory
+    '/:id',
+    fileUpload(fileValidation.image).single('image'),
+    categoriesController.updatecategory
 );
 
 export default router;
